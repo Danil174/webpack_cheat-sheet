a2f43137b8af2090cc03ce2598bf747594791268
danil milostiviy <milostiviy.danil@yandex.ru>
2020-08-23 17:46:46 +0300

    add cross-env

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV  === 'development';
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -26,7 +29,8 @@ module.exports = {
         } 
     },
     devServer: {
-        port: 4200
+        port: 4200,
+        hot: isDev,
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -38,12 +42,24 @@ module.exports = {
                 {from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist')},
             ]
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [
+                    {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: {
+                        hmr: isDev,
+                        reloadAll: true
+                      },
+                    },
+                    'css-loader',
+                ],
             },
             {
                 test: /\.(png|jpg|svg|gif)$/,
