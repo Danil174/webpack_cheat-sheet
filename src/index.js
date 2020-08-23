c4df71eef1b05986308e34797af5f837ff623458
danil milostiviy <milostiviy.danil@yandex.ru>
2020-08-23 15:46:44 +0300

    imgs and fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Post from './post.js';
 import './styles/styles.css';
+import Ava from './assets/avatar.jpg';
 
-const post = new Post(`new webpack post title`);
+const post = new Post(`new webpack post title`, Ava);
 
 console.log(`Post to String`, post.toString());
--- src/post.js
+++ src/post.js
@@ -1,12 +1,14 @@
 export default class Post {
-    constructor(title) {
+    constructor(title, img) {
         this.title = title;
+        this.ava = img;
         this.date = new Date();
     }
 
     toString() {
         return JSON.stringify({
             title: this.title,
+            ava: this.ava,
             date: this.date.toJSON()
         })
     }
