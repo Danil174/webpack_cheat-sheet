d4594cf69122d57370557e23ce84fcb561a3a9ef
danil milostiviy <milostiviy.danil@yandex.ru>
2020-08-23 17:58:20 +0300

    prod optimization

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const isDev = process.env.NODE_ENV  === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        } 
+    }
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin(),
+        ]
+    }
+    return config;
+};
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -23,18 +41,17 @@ module.exports = {
             '@': path.resolve(__dirname, 'src'),
         }
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        } 
-    },
+    optimization: optimization(),
     devServer: {
         port: 4200,
         hot: isDev,
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
         }),
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin({
