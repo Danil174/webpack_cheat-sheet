f3cc129b30d2f2b18e99d061a23fd62ef31e8f31
danil milostiviy <milostiviy.danil@yandex.ru>
2020-08-23 16:08:21 +0300

    more loaders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import Post from './post.js';
 import './styles/styles.css';
 import Ava from './assets/avatar.jpg';
+// import xml from './assets/data.xml';
+// import csv from './assets/data.csv';
 
 const post = new Post(`new webpack post title`, Ava);
 
 console.log(`Post to String`, post.toString());
+
+// console.log('csv', csv);
+// console.log(`xml`, xml);
